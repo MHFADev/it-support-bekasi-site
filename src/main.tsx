8968eb13d9fb15afdb818acf5b9bab981a9bdbe3
MHCA CAT <hilmi.firjatullah12@gmail.com>
2026-01-23 10:11:46 +0700

    [Blink] Fix Vercel build error - Remove .tsx extension from App import • Changed `import App from './App.tsx'` to `import App from './App'` in main.tsx • Some bundlers (like Rollup used by Vercel) cannot resolve explicit .tsx extensions • This fix allows the build to complete successfully on Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { BlinkProvider } from '@blinkdotnew/react'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 function getProjectId(): string {
